3c182b91ca929fd2050420dbc3282605e76f60e2
Johannaguilar <johannaguilareyno@gmail.com>
2025-09-21 20:53:18 -0500

    implementado recuperar funciones int y float

--- utilitarios/utilitarios.js
+++ utilitarios/utilitarios.js
@@ -1,9 +1,21 @@
 saludar=function(){
     let nombre=recuperarTexto("txtNombre");
     let apellido=recuperarTexto("txtApellido");
+    let edad=recuperarInt("txtEdad");
+    let statura=recuperarFloat("txtEstatura");
 
 }
 recuperarTexto=function(idComponente){
     let valorIngresado=document.getElementById(idComponente).value;
     return valorIngresado;
+}
+recuperarInt=function(idComponente){
+    let valorCaja= recuperarTexto(idComponente);
+    let valorEntero= parseInt(valorCaja);
+    return valorEntero;
+}
+recuperarFloat=function(idComponente){
+    let valorCaja= recuperarTexto(idComponente);
+    let valorFlotante= parseFloat(valorCaja);
+    return valorFlotante;
 }

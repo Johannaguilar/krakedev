49146c326d4e4e9a4945392410f8d14175230044
Johannaguilar <johannaguilareyno@gmail.com>
2025-09-22 21:15:33 -0500

    calcular subtotal y mostrarlo en pantalla funcionando

---
+++ puntoDeVenta_v4/serviciosPuntoDeVenta.js
@@ -0,0 +1,13 @@
+calcularValorDescuento=function(monto,porcentajeDescuento){
+    return (monto * porcentajeDescuento )/100;
+}
+calcularIVA=function(monto){
+    return (monto*12)/100;
+}
+calcularSubTotal=function(precio,cantidad){
+    return precio*cantidad;
+}
+calcularTotal=function(subtotal,descuento,iva){
+    return subtotal-descuento+iva;
+}
+

ea69491c4073187a48edd97ecff6791c6dc8cbfb
Johannaguilar <johannaguilareyno@gmail.com>
2025-09-23 14:28:22 -0500

    todo funcionando

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -1,3 +1,10 @@
 calcularPromedioNotas=function(){
-    
+    let n1=recuperarFlotante("nota1");
+    let n2=recuperarFlotante("nota2");
+    let n3=recuperarFlotante("nota3");
+    let promedio=calcularPromedio(n1,n2,n3);
+    let promedioModificado=promedio.toFixed(2);
+    cambiarTexto("lblpromedio",promedioModificado);
+
+
 }

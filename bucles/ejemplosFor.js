667a9cf7ce14da00ae8df21b785ca196b04a0083
Johannaguilar <johannaguilareyno@gmail.com>
2025-10-02 17:10:42 -0500

    funcion mostrarImpares funcionando reto37

---
+++ bucles/ejemplosFor.js
@@ -0,0 +1,54 @@
+mostrarNumero=function(){
+    console.log("antes del for");
+    for(let porcentaje=0;porcentaje<4;porcentaje++){
+        console.log(porcentaje);
+    }
+
+
+    console.log("despues del for");
+}
+mostrarNumero2=function(){
+    console.log("antes del for");
+    for(let indice=1;indice<=5;indice++){
+        console.log(indice);
+    }
+
+
+    console.log("despues del for");
+}
+mostrarPares=function(){
+    console.log("antes del for");
+    for(let impar=2;impar<=10;impar+=2){
+        console.log(impar);
+    }
+
+
+    console.log("despues del for");
+}
+mostrarInverso=function(){
+    console.log("antes del for");
+    for(let porcentaje=10;porcentaje>=0;porcentaje--){
+        console.log(porcentaje);
+    }
+
+
+    console.log("despues del for");
+}
+hackearNadaPelis=function(){
+    for(let porcentaje=0;porcentaje<=100;porcentaje+=10){
+        console.log("hackeando nasa"+porcentaje+"%");
+    }
+
+
+    console.log("LA NASA A SIDO HACKEADA XD");
+}
+// reto 37
+mostrarImpares=function(){
+    console.log("antes del for");
+    for(let impar=1;impar<=21;impar+=2){
+        console.log(impar);
+    }
+
+
+    console.log("despues del for");
+}

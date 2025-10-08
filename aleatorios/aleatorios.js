1fdf36fa86709260e392dc7c957bb4d211763172
Johannaguilar <johannaguilareyno@gmail.com>
2025-10-08 13:16:52 -0500

    reto 42 todo funcionando

--- aleatorios/aleatorios.js
+++ aleatorios/aleatorios.js
@@ -10,11 +10,28 @@ aleatorio=function(){
 }
 generarAleatorios=function(){
     let aleatorios=[];
+    let numAleatorio;
     let valorTxt=recuperarInt("txtCaja");
     if(valorTxt>=5&&valorTxt<=20){
         for(let i=0;i<valorTxt;i++){
             console.log(i);
+            numAleatorio=aleatorio();
+            aleatorios+=numAleatorio;
         }
+        mostrarResultado(aleatorios);
     }
 
+}
+mostrarResultado=function(arregloNumeros){
+    let cmpTabla=document.getElementById("divTabla");
+    let contenidoTabla="<table><tr><th>NUMERO ALEATORIO</th></tr>";
+    let numeroAleatorio;
+    for(let i=0;i<arregloNumeros.length;i++){
+        numeroAleatorio=arregloNumeros[i];
+        contenidoTabla+="<tr><td>";
+        contenidoTabla+=numeroAleatorio;
+        contenidoTabla+="</tr></td>";
+    }
+    contenidoTabla+="</table>";
+    cmpTabla.innerHTML=contenidoTabla;
 }
